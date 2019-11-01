0609e681fe4c1bd938b09ef5a115bb7b643bacce
Alexander Troost <alexander.troost@kpn.com>
2019-11-01 20:56:54 +0100

    first release

---
+++ avr.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"crypto/rand"
+	"crypto/tls"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func handleConnection(c net.Conn) {
+	log.Printf("Serving %s\n", c.RemoteAddr().String())
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		temp := strings.TrimSpace(string(netData))
+		if temp == "STOP" {
+			break
+		}
+	}
+	c.Close()
+}
+
+// only needed below for sample processing
+
+func main() {
+	// create log
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	// check certs
+	cert, err := tls.LoadX509KeyPair("certs/server.pem", "certs/server.key")
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	// verify port is part of command
+	arguments := os.Args
+	if len(arguments) == 1 {
+		log.Printf("Please provide a port number!")
+		return
+	}
+
+	log.Printf("Launching server...")
+	// define port
+	PORT := ":" + arguments[1]
+	// listen on all interfaces
+	config := tls.Config{Certificates: []tls.Certificate{cert}}
+	config.Rand = rand.Reader
+	ln, err := tls.Listen("tcp", PORT, &config)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// run loop forever (or until ctrl-c)
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		go handleConnection(c)
+	}
+}
