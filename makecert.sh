9b80a68f38a68293b374271ff26c8b5f8e6f7bb7
Alexander Troost <alexander.troost@kpn.com>
2019-11-03 16:40:00 +0100

    Removed client generation, email address and added KSP bits priv key

--- makecert.sh
+++ makecert.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # call this script with an email address (valid or not).
 # like:
-# ./makecert.sh alexander.troost@kpn.com
+# ./makecert.sh
 mkdir certs
 rm certs/*
 echo "make server cert"
-openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 365 -subj "/C=NL/ST=ZH/L=Rotterdam/O=KPN N.V./OU=CTDO SP TV/CN=avr.stb.tv.kpn.com/emailAddress=$1"
-echo "make client cert"
-openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 365 -subj "/C=NL/ST=ZH/L=Rotterdam/O=KPN N.V./CN=avr.stb.tv.kpn.com/emailAddress=$1"
+openssl req -newkey rsa:4096 -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 365 -subj "/C=NL/ST=ZH/L=Rotterdam/O=KPN N.V./OU=CTDO SP TV/CN=avr.stb.tv.kpn.com"
