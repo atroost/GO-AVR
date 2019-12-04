1dec13a7e367f3189d3a4a477097891a2191f151
Gabor <gabor.de.wit@accenture.com>
2019-12-04 13:34:01 +0100

    Apply most recent KSP configuration for TLS traffic - https://github.com/KPN-CISO/kpn-security-policy/blob/5d7ffee6a9ed2652034fc766da540312689fa7be/05.%20System%20and%20Network%20Security/5%20-%20Cryptography%20Algorithms%20and%20Cipher%20Suites.xlsx

--- avrWithSSL.go
+++ avrWithSSL.go
@@ -24,7 +24,20 @@ func startAvrSecure(launchWithChannel bool) {
 	}
 
 	// Construct secure TCP server - TODO make naming convention the same over the  two files
-	config := tls.Config{Certificates: []tls.Certificate{cert}}
+	config := tls.Config{
+		Certificates: []tls.Certificate{cert},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		},
+	}
 	config.Rand = rand.Reader
 	secureTcpServer, err := tls.Listen("tcp", configPortSecureTcp, &config)
 	if err != nil {
